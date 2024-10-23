4a61eb07be45d193831b16847e37acbc0e7322d9
Basstler1 <tjmo@post.pl>
2024-10-23 21:50:20 +0200

    Text...

---
+++ src/exercise/Marmolek.java
@@ -0,0 +1,14 @@
+package exercise;
+// it's mine!!! TMoskwa
+public class Marmolek {
+
+		/* Hier sollte was kommen, die den Aufgaben entspricht
+		 * wurde aber von meine Seite nicht geschafft.
+		 * Prinzipiell: wenn ich was nicht verstehe, dann verstehe ich nicht
+		 * Punkt und Tchüss.
+		 */
+	
+ }
+ 
+ 
+}
