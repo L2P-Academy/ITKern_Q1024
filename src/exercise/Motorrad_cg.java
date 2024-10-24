045e7b2c13b539af4c26dbb8b8e936bcecc47291
Kaspercg <stebanko54@gmail.com>
2024-10-24 15:14:27 +0200

    Aufgabe 12: method for print infos, some objects, some data structure

--- src/exercise/Motorrad_cg.java
+++ src/exercise/Motorrad_cg.java
@@ -37,13 +37,52 @@ public class Motorrad_cg {
 		this.marke = marke;
 		this.leistung = leistung;
 	}
-	public void getBikeInformation() {
+	public Motorrad_cg() {}
+	public void print() {
 		System.out.println(this.toString());
+	}
+	public void getBikeInformation() {
+		print();
 	}
 	@Override
 	public String toString() {
-		return "Motorrad_cg [kmStand=" + kmStand + ", bauJahr=" + bauJahr + ", marke=" + marke + ", leistung="
-				+ leistung + "]";
+		return "Motorrad_cg [\t\nKilometerstand=" + kmStand + ",\t\n Baujahr=" + bauJahr + ",\t\n Marke=" + marke + ",\t\n Leistung="
+				+ leistung + " ps\n]";
 	}
-	
+	public void printWithTab() {
+		System.out.println("\tBaujahr: "+this.getBauJahr());
+		System.out.println("\tKilometerstand: "+this.getKmStand());
+		System.out.println("\tLeistung: "+this.getLeistung()+" ps");
+		System.out.println("\tMarke: "+this.getMarke());
+	}
+	public void beispiel() {
+		System.out.println();
+		System.out.println("Meine Wunschliste:");
+		System.out.println();
+		Motorrad_cg obj1 = new Motorrad_cg(
+				15000,
+				2010,
+				"bmw",
+				50
+				);
+		obj1.print();
+		obj1.setBauJahr(1990);
+		obj1.setKmStand(1000);
+		obj1.setLeistung(40);
+		obj1.setMarke("abc");
+		obj1.print();
+		Motorrad_cg obj2 = new Motorrad_cg(
+				150000,
+				2020,
+				"yamaha",
+				60
+				);
+		obj2.print();
+		System.out.println();
+		System.out.println("Motorrad 1:");
+		obj1.printWithTab();
+		System.out.println();
+		System.out.println("Motorrad 2:");
+		obj2.printWithTab();
+	}
 }
