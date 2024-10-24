83254a84afaf9381f0e4002d2ad1bf5fd8dcbf45
Basstler1 <tjmo@post.pl>
2024-10-24 20:03:11 +0200

    Let's see

--- src/launcher/Main.java
+++ src/launcher/Main.java
@@ -8,19 +8,34 @@ import java.util.List;
 import exercise.Auto;
 import exercise.CB_Rocco;
 import exercise.Example;
+import exercise.Motorrad_tm;
 
 // DO NOT PUSH THIS CLASS
 public class Main {  
 
 	public static void main(String[] args) {
 
+		/*
 		Auto bmw = new Auto(10000, 1989, "bmw", Color.BLACK); // Objekt erzeugen
 		System.out.println("Alter Stand:" + bmw.getKmStand());
 		bmw.setKmStand(bmw.getKmStand() + 500);
 		System.out.println(bmw.getFarbe()); // hole die aktuelle Farbe
 		bmw.setFarbe(Color.BLUE); // ändere die farbe (setzen) auf blau
 		System.out.println(bmw.getFarbe()); // hole die aktuelle Farbe
-
+		*/
+		
+		Motorrad_tm kawasaki = new Motorrad_tm(20000, 2000, "Kawasaki", 125);
+		System.out.println(kawasaki.getLeistung());
+		kawasaki.setLeistung(500);
+		System.out.println(kawasaki.getLeistung());
+		kawasaki.setKmStand(kawasaki.getKmStand() + 15000);
+		System.out.println(kawasaki.getKmStand());
+		
+		kawasaki.showBikeInformation();
+		
+		
+		
+				
 		}
 
 }
