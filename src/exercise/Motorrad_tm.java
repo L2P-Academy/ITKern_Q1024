b9b7949c4b63b7a729069f2ec151d4deed3da973
Basstler1 <tjmo@post.pl>
2024-10-27 21:58:20 +0100

    Nicht funktionierte Versuch

--- src/exercise/Motorrad_tm.java
+++ src/exercise/Motorrad_tm.java
@@ -6,7 +6,12 @@ public class Motorrad_tm {
 	int kmStand;
 	int bauJahr;
 	String marke;
-	int leistung;
+	int leistungKw;
+	int leistungPS;
+	double doubleKmStand = kmStand;
+	double doubleBauJahr = bauJahr;
+	double psFactor = 1.6;
+	double kwFactor = 0.746;
 	
 	// get & set
 	public int getKmStand() {
@@ -27,27 +32,57 @@ public class Motorrad_tm {
 	public void setMarke(String marke) {
 		this.marke = marke;
 	}
-	public int getLeistung() {
-		return leistung;
+	public int getLeistungKw() {
+		return leistungKw;
 	}
-	public void setLeistung(int leistung) {
-		this.leistung = leistung;
+	public void setLeistungKw(int leistungKw) {
+		this.leistungKw = leistungKw;
 		}
-
+	public double getDoubleKmStand() {
+		return doubleKmStand;
+	}
+	public void setDoubleKmStand(double doubleKmStand) {
+		this.doubleKmStand = doubleKmStand;
+	}
+	public double getDoubleBauJahr() {
+		return doubleBauJahr;
+	}
+	public void setDoubleBauJahr(double doubleBauJahr) {
+		this.doubleBauJahr = doubleBauJahr;
+	}
+	public int getLeistungPS() {
+		return leistungPS;
+	}
+	public void setLeistungPS(int leistungPS) {
+		this.leistungPS = leistungPS;
+	}
 	// Konstruktor
-	public Motorrad_tm(int kmStand, int bauJahr, String marke, int leistung) {
+	public Motorrad_tm(int kmStand, int bauJahr, String marke, int leistungKw) {
 		this.kmStand = kmStand;
 		this.bauJahr = bauJahr;
 		this.marke = marke;
-		this.leistung = leistung;
+		this.leistungKw = leistungKw;
 	}
-	
+	// Umrechnung kW-PS
+	public double calcKwToPS (double ps) {
+		double result = leistungKw * psFactor;
+		return result;
+	}
+	// Umrechnung PS-Kw
+	public double calcPsToKw (double kw) {
+		double result = (??) * kwFactor;
+		return result;
+	}
 	// Methode
 	public void showBikeInformation() {
 		System.out.println("Kilometerstand:" + getKmStand());
+		System.out.println("Kilometerstand:" + getDoubleKmStand());
 		System.out.println("Baujahr: " + getBauJahr());
+		System.out.println("Baujahr: " + getDoubleBauJahr());
 		System.out.println("Marke: " + getMarke());
-		System.out.println("Leistung: " + getLeistung() + ("kW"));
-		}
+		System.out.println("Leistung: " + getLeistungKw() + ("kW"));
+		//System.out.println("Leistung: " + calcKwToPS() + ("PS"));
+		System.out.println(ps);
+	}
 }
 
