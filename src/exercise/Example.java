4ad3ff84b81369104d27812ecd815755b1cee780
L2P-Academy <projekte@l2p-academy.com>
2024-10-28 14:12:41 +0100

    Demonstration of Strings and equals

--- src/exercise/Example.java
+++ src/exercise/Example.java
@@ -50,16 +50,96 @@ public class Example {
 		
 		System.out.println("Ein String aus einem Char: " + charToString3);
 		
-		// Strings
-		// absoluter Pfad:
-		String absoluterPfad = "C:/user/name/.."; // -> führt öfter zu Problemen
-		// relativer Pfad:
-		String relativerPfad = "res/meinFoto.png";		
-		
 		// komplexe Datentypen
 		String text1 = new String("abc");		
 		// oder 
-		String text2 = "abc"; // -> ohne new klappt nur mit Strings!!!		
+		String text2 = "abc"; // -> ohne new klappt nur mit Strings!!!	
+		
+		// Strings und String-Operationen
+		// absoluter Pfad:
+		String absoluterPfad = "C:/user/name/.."; // -> führt öfter zu Problemen
+		// relativer Pfad:
+		String relativerPfad = "res/meinFoto.png";
+		
+		// Textlänge + Position (Index)
+		String longText = " Das ist ein Text mit vielen mit Buchstaben ";
+		System.out.println(longText);
+		System.out.println("Die Textlänge ist: " + longText.length() + " Zeichen");
+		System.out.println("Caps-Lock:" + longText.toUpperCase());
+		System.out.println("Alles klein: " + longText.toLowerCase());
+		System.out.println(longText.indexOf("mit")); // erstes Auftreten
+		System.out.println(longText.lastIndexOf("mit")); // letztes Auftreten
+		
+		// "Konkatenation" = Anhängen
+		// Variante 1: concat
+		System.out.println(longText.concat(" und einem angehangenen Text!"));
+		// Variante 2: +
+		System.out.println(longText + " und einem angehangenen Text!");
+		
+		// Leerzeichen bzw. Zeichen entfernen/ersetzen
+		System.out.println(longText.trim() + "ohne führende/abschließende Leerzeichen");
+		// mit char
+		System.out.println(longText.replace('e', 'a')); // alle e zu a (als char)
+		// mit String
+		System.out.println(longText.replace("a", "e")); // alle a zu e (als String)
+		System.out.println(longText);
+		// alle Leerzeichen entfernen
+		System.out.println("Text ohne Leerzeichen: " + longText.replace(" ", "")); // alle Leerzeichen zu "nichts"
+		
+		System.out.println("Text beinhaltet Leerzeichen: " + longText.contains("Das")); // prüfe, ob die Zeichen vorkommen (true/false)
+		System.out.println("Text leer: " + longText.isEmpty());
+		
+		// Vergleiche von Strings
+		String shortText = new String("Kurzbeschreibung");
+		System.out.println("Textinhalt vergleichen: " + shortText.equals(longText)); // vergleicht die Inhalte der Strings
+		
+		// Bedingungen & Vergleichsoperatoren
+		int a = 5;
+		int b = 10;
+		
+		// größer/kleiner/gleich
+		if (a < b) {			
+		}		
+		if (a >= b) {			
+		}
+		
+		// Und && 
+		if (a == b && b > 5 && a > 2) {			
+		}
+		
+		// Oder ||
+		if (a == b || b > 5) {			
+		}
+		
+		// Negation
+		if (a != b) { // a ist ungleich b
+			System.out.println("A ist ungleich B (einzeln)");
+		}
+		if (!(a==b)) { // negation eines Ausdrucks
+			System.out.println("A ist ungleich B (gesamt)");
+		}
+		
+		// NAND
+		if (!(a == b && b > 5)) {			
+		}
+		
+		// NOR
+		if (!(a == b || b > 5)) {			
+		}
+		
+		// komplexe Vergleiche
+		String text = "";
+		if (text.length() == 0) {			
+		}
+		
+		if (text.isEmpty() && !shortText.isEmpty()) {
+			System.out.println("Text1 ist leer, Text2 nicht");
+		} else if (!text.isEmpty()) {
+			System.out.println("Text1 ist nicht leer");
+		} else {
+			System.out.println("Bedingung nicht aufgelistet - alle anderen Fälle");
+		}
+		
 		
 		List<String> list = new ArrayList<>();
 		HashMap<String, String> hauptStaedte = new HashMap<>();
